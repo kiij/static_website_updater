c1ebc8426a35fbeec408379ee2b5f1c32696b343
kiij <arewereally@gmail.com>
2015-11-07 00:46:11 -0800

    Tested & working

--- setup.py
+++ setup.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name="static_website_updater",
     version="1",
-    packages=find_packages(),
+    packages=["static_website_updater"],
 
     install_requires=[
         "boto",
         "flickrapi",
-        "GitPython"
-    ]
+        "GitPython",
+        "requests>=2.5.1",
+    ],
+    entry_points={
+        "console_scripts": [
+            "static_website_updater = static_website_updater.main:main"
+        ]
+    },
 )
--- static_website_updater/website/jekyll_website.py
+++ static_website_updater/website/jekyll_website.py
@@ -19,7 +19,8 @@ class JekyllWebsite(object):
     def build(self):
         orig_wd = os.getcwd()
         os.chdir(self.get_path())
-        call(["jekyll", "build"])
+        call(["bundle", "install"])
+        call(["bundle", "exec", "jekyll", "build"])
         os.chdir(orig_wd)
 
         return os.path.join(self.get_path(), "_site")
